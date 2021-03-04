81f212fa2958c6011320b7d5331ec7c9927a76b3
Elodie Dalmon <elodie.dalmon@gmail.com>
2021-03-04 16:38:28 +0100

    routes / controller / avant css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "create ingredients"
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "aperol")
+Ingredient.create(name: "prosecco")
+Ingredient.create(name: "wodka")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "orange juice")
+Ingredient.create(name: "tomato juice")
+
+puts "#{Ingredient.count}"
+
+# puts "create cocktails"
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+
+# puts "#{Cocktail.count}"
